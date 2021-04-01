5cef08f7e10d4100c49d2695b593878b2a229f9a
iyyappan24 <ablyiyyappan360@gmail.com>
2021-04-01 23:41:39 +0530

    added lambu.java

---
+++ lambu.java
@@ -0,0 +1,35 @@
+package xam;
+
+interface voca{
+	public String show();
+}
+
+
+public class lambu {
+
+	public static String allow(){
+		return "lini";
+	}
+	
+	
+	public static void push(voca v)
+	{
+            	String res =v.show();	
+	            System.out.println(res);
+	}
+	
+	public static void main(String[] args){
+		
+		
+		voca vo = lambu :: allow;
+		
+		lambu.push(vo);
+		
+	}
+	
+	
+	
+	
+	
+	
+}
